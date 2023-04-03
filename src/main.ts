67bf4ba4341be67ed56375c607f0c6f1de3cb581
Samar <samarbadriddinov.tv@gmail.com>
2023-04-03 10:06:32 +0200

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ['error'] });
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(8000);
+  await app.listen(parseInt(process.env.PORT) || 8000);
 }
 bootstrap();
