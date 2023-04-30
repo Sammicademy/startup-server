298f17ed0bb598db8092bbe11f729350a6e70ba1
Samar <samarbadriddinov.tv@gmail.com>
2023-04-30 19:26:27 +0300

    #115. Filter all instructors

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -8,19 +8,9 @@ export class AdminService {
   constructor(@InjectModel(Instructor.name) private instructorModel: Model<InstructorDocument>) {}
 
   async getAllInstructors() {
-    const approvedInstructor = await this.instructorModel
-      .find({ approved: true })
-      .populate('author')
-      .exec();
-    const appliedInstructor = await this.instructorModel
-      .find({ approved: false })
-      .populate('author')
-      .exec();
+    const instructors = await this.instructorModel.find().populate('author').exec();
 
-    return {
-      approved: approvedInstructor.map(instructor => this.getSpecificField(instructor)),
-      applied: appliedInstructor.map(instructor => this.getSpecificField(instructor)),
-    };
+    return instructors.map(instructor => this.getSpecificField(instructor));
   }
 
   getSpecificField(instructor: InstructorDocument) {
